0037a4e6e03c79ef0d944e7223cdb64bce123507
Yusuf Savaş <yusufsss4@gmail.com>
2023-10-28 22:57:35 +0300

    refactor: add comments

--- ai_tag/src/modules/ai_module.c
+++ ai_tag/src/modules/ai_module.c
@@ -1,18 +1,32 @@
-#include <zephyr/kernel.h>
+/**
+ * @brief AI module responsible for all AI related events.
+ *
+ * @file ai_module.c
+ *
+ */
+
+/***********************************************************************************************************************
+ * Header Includes
+ **********************************************************************************************************************/
+
+#define MODULE ai_module
+
+// Standard Libraries
 #include <stdio.h>
+
+// Third-party Libraries
+#include <zephyr/kernel.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
 #include <app_event_manager.h>
-
-#if defined(CONFIG_EXTERNAL_SENSORS)
-#include "ext_sensors.h"
-#endif
-
 #if defined(CONFIG_ADP536X)
 #include <adp536x.h>
 #endif
 
-#define MODULE ai_module
-
+// PTAI Libraries
+#if defined(CONFIG_EXTERNAL_SENSORS)
+#include "ext_sensors.h"
+#endif
 #include "modules_common.h"
 #include "events/app_module_event.h"
 #include "events/data_module_event.h"
@@ -21,9 +35,18 @@
 
 #include "ai/ai_analyzer.h"
 
-#include <zephyr/logging/log.h>
+/***********************************************************************************************************************
+ * Macro Definitions
+ **********************************************************************************************************************/
 LOG_MODULE_REGISTER(MODULE, CONFIG_AI_MODULE_LOG_LEVEL);
 
+/* Sensor module message queue. */
+#define AI_QUEUE_ENTRY_COUNT 10
+#define AI_QUEUE_BYTE_ALIGNMENT 4
+
+/***********************************************************************************************************************
+ * Typedef Definitions
+ **********************************************************************************************************************/
 struct ai_msg_data
 {
 	union
@@ -41,10 +64,6 @@ static enum state_type {
 	STATE_SHUTDOWN
 } state;
 
-/* Sensor module message queue. */
-#define AI_QUEUE_ENTRY_COUNT 10
-#define AI_QUEUE_BYTE_ALIGNMENT 4
-
 K_MSGQ_DEFINE(msgq_ai, sizeof(struct ai_msg_data),
 			  AI_QUEUE_ENTRY_COUNT, AI_QUEUE_BYTE_ALIGNMENT);
 
@@ -53,8 +72,21 @@ static struct module_data self = {
 	.msg_q = &msgq_ai,
 	.supports_shutdown = true,
 };
+/***********************************************************************************************************************
+ * Private Global Variables
+ **********************************************************************************************************************/
 
-/* Convenience functions used in internal state handling. */
+/***********************************************************************************************************************
+ * Private Function Definitions
+ **********************************************************************************************************************/
+/**
+ * @brief Convert a state enum to a string representation.
+ *
+ * This function takes a state enum and returns a string representation.
+ *
+ * @param new_state The state enum to convert.
+ * @return A string representation of the state.
+ */
 static char *state2str(enum state_type new_state)
 {
 	switch (new_state)
@@ -70,6 +102,13 @@ static char *state2str(enum state_type new_state)
 	}
 }
 
+/**
+ * @brief Set the state of the application.
+ *
+ * This function sets the state of the application and logs a state transition message.
+ *
+ * @param new_state The new state to set.
+ */
 static void state_set(enum state_type new_state)
 {
 	if (new_state == state)
@@ -85,7 +124,14 @@ static void state_set(enum state_type new_state)
 	state = new_state;
 }
 
-/* Handlers */
+/**
+ * @brief Event handler for the application module.
+ *
+ * This function handles application events and enqueues messages for further processing.
+ *
+ * @param aeh The pointer to the application event header.
+ * @return True when a subscribed events comes else false.
+ */
 static bool app_event_handler(const struct app_event_header *aeh)
 {
 	struct ai_msg_data msg = {0};
@@ -129,8 +175,15 @@ static bool app_event_handler(const struct app_event_header *aeh)
 	return false;
 }
 
-/* Static module functions. */
-
+/**
+ * @brief Event handler for external sensor events.
+ *
+ * This function handles events related to external sensors and logs errors.
+ * 
+ * @todo Delete if not needed.
+ *
+ * @param evt The external sensor event.
+ */
 #if defined(CONFIG_EXTERNAL_SENSORS)
 static void ext_sensor_handler(const struct ext_sensor_evt *const evt)
 {
@@ -157,6 +210,11 @@ static void ext_sensor_handler(const struct ext_sensor_evt *const evt)
 }
 #endif /* CONFIG_EXTERNAL_SENSORS */
 
+/**
+ * @brief Submits the AI analysis results data as a module event. @c ai_module_event
+ *
+ * This function retrieves AI analysis results data, processes it, and enqueues the results for further processing.
+ */
 static void ai_analysis_result_data_get(void)
 {
 	struct ai_module_event *ai_module_event;
@@ -213,6 +271,13 @@ static void ai_analysis_result_data_get(void)
 	APP_EVENT_SUBMIT(ai_module_event);
 }
 
+/**
+ * @brief Initialize the module.
+ *
+ * This function initializes the module and external sensors if configured.
+ *
+ * @return 0 on success, or a negative error code on failure.
+ */
 static int setup(void)
 {
 #if defined(CONFIG_EXTERNAL_SENSORS)
@@ -228,6 +293,15 @@ static int setup(void)
 	return 0;
 }
 
+/**
+ * @brief Check if AI analysis result data is requested.
+ *
+ * This function checks if AI analysis result data is requested in the data list.
+ *
+ * @param data_list The list of data types.
+ * @param count The number of data types in the list.
+ * @return True if AI analysis result data is requested, otherwise false.
+ */
 static bool ai_analysis_result_data_requested(enum app_module_data_type *data_list,
 											  size_t count)
 {
@@ -242,13 +316,25 @@ static bool ai_analysis_result_data_requested(enum app_module_data_type *data_li
 	return false;
 }
 
-/* Message handler for STATE_INIT. */
+/**
+ * @brief Message handler for STATE_INIT.
+ *
+ * This function handles messages specific to the STATE_INIT state.
+ *
+ * @param msg The AI message data.
+ */
 static void on_state_init(struct ai_msg_data *msg)
 {
 	state_set(STATE_RUNNING);
 }
 
-/* Message handler for STATE_RUNNING. */
+/**
+ * @brief Message handler for STATE_RUNNING.
+ *
+ * This function handles messages specific to the STATE_RUNNING state.
+ *
+ * @param msg The AI message data.
+ */
 static void on_state_running(struct ai_msg_data *msg)
 {
 	if (IS_EVENT(msg, app, APP_EVT_DATA_GET))
@@ -260,7 +346,13 @@ static void on_state_running(struct ai_msg_data *msg)
 	}
 }
 
-/* Message handler for all states. */
+/**
+ * @brief Message handler for all states.
+ *
+ * This function handles messages common to all states.
+ *
+ * @param msg The AI message data.
+ */
 static void on_all_states(struct ai_msg_data *msg)
 {
 	if (IS_EVENT(msg, util, UTIL_EVT_SHUTDOWN_REQUEST))
@@ -273,6 +365,15 @@ static void on_all_states(struct ai_msg_data *msg)
 	}
 }
 
+/**
+ * @brief AI Module thread function.
+ *
+ * This function is the main thread function for the AI module. It initializes the module,
+ * handles state transitions, processes messages, and responds to various events. The module
+ * continuously runs in a loop to execute its tasks.
+ *
+ * @note This function runs indefinitely in a separate thread.
+ */
 static void module_thread_fn(void)
 {
 	int err;
@@ -319,7 +420,13 @@ static void module_thread_fn(void)
 		on_all_states(&msg);
 	}
 }
+/***********************************************************************************************************************
+ * Public Function Definitions
+ **********************************************************************************************************************/
 
+/***********************************************************************************************************************
+ * Module Thread Definitions
+ **********************************************************************************************************************/
 K_THREAD_DEFINE(ai_module_thread, CONFIG_AI_THREAD_STACK_SIZE,
 				module_thread_fn, NULL, NULL, NULL,
 				K_LOWEST_APPLICATION_THREAD_PRIO, 0, 0);
